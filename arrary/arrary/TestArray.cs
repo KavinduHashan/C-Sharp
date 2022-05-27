df87d6d7425632064ecccfe1356febceda97a7c7
Kavindu Hashan <89184906+KavinduHashan@users.noreply.github.com>
2022-05-27 12:37:00 +0530

    Add files via upload

---
+++ arrary/arrary/TestArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class TestArray
+    {
+        public void MyArray()
+        {
+            //declaration
+            int[] array = new int[10];
+            int i;
+
+            //input
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("Enter value for array ({0} index) : ", i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
